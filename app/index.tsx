5f7428b254d175b713c0d236d5c56e9cdf9c2ddb
carlos22-R <rc19102@ues.edu.sv>
2024-06-29 12:30:17 -0600

    boton en el index

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { View , Text,Button, TextInput} from "react-native";
+import { View , Text,Button, TextInput, TouchableOpacity,StyleSheet} from "react-native";
 import { useContext } from "react";
 import AppContext from "./AppContext";
 const HomePage =() =>{
@@ -17,8 +17,37 @@ const HomePage =() =>{
         <Link href="/users/tabla">Go to user 1 </Link>
         <Link href="/users/Prueba">Go to Prueba </Link>
         <Link href="/users/resultado">Go to Resultado </Link>
-      
+        <View style={styles.containerb}>
+        
+        <TouchableOpacity style={styles.button} >
+        <Link href="/users/Prueba"> 
+          <Text style={styles.buttonText}>
+            Comenzar</Text>
+        </Link>
+        </TouchableOpacity>
+        
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  containerb: {
+  justifyContent: 'center',
+  alignItems: 'flex-end',
+  marginRight:10,
+  marginBottom:10
+  },
+
+  button: {
+    backgroundColor: 'green',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+
+});
 export default HomePage;
